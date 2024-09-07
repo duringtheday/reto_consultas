ed8914b31855b994f529f4c47d6f3005a8f05dcd
D_t_D <132130896+duringtheday@users.noreply.github.com>
2024-09-07 03:29:37 -0500

    Reto: Consultas
    Academia-X

---
+++ script.js
@@ -0,0 +1,8 @@
+// Reto: Consultas
+// 1. Obtén el elemento header, un botón de navegación, una 
+// tarjeta y el footer de una página web utilizando el DOM.
+// 2. Intenta escribir el código de HTML, CSS y JS que ves 
+// en esta página renderizada. No tiene que ser perfecto! 🧑‍💻
+document.getElementsByClassName('header');
+document.getElementsById('inicio');
+document.getElementsByTagName('footer');
